74224591af03efc60462902b1f86473c25a7c70a
RaphiKumbi <RaphiKumbi@gmail.com>
2026-01-02 21:20:53 +0100

    feat: favicon

---
+++ website/client/src/page/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./content/Header.jsx";
+import Footer from "./content/Footer.jsx";
+
+function App() {
+    return (
+      <>
+        <Header></Header>
+        <h1>Content</h1>
+        <Footer></Footer>
+      </>
+    )
+}
+
+export default App
---
+++ website/client/src/page/content/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className="footer">
+        <h3>The Mulenga Farm</h3>
+        <p>
+            <a href="mailto:themulengafarm@proton.me">
+                themulengafarm@proton.me
+            </a>
+        </p>
+        <p>© 2024 The Mulenga Farm. All Rights Reserved.</p>
+    </footer>
+);
+
+export default Footer;
