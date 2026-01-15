8275d51e2d2d5a197ae174acf558ace93d063307
RaphiKumbi <RaphiKumbi@gmail.com>
2026-01-15 14:33:38 +0100

    chore: routes test

--- website/server.js
+++ website/server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
