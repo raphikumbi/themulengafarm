bd8bd372be591ca5b128aa83d75b9cb79dce9022
raphakumba <raphikumbi@gmail.com>
2026-04-24 17:38:46 +0200

    fix: pictures

--- website/client/src/pages/pictures/PicturePage.jsx
+++ website/client/src/pages/pictures/PicturePage.jsx
@@ -5,46 +5,28 @@ import "./PicturePage.css";
 
 export default function PicturePage() {
     const imgPath = (name) =>
-        `/photos/photos2025/${name.replace(".JPG", ".webp")}`;
+        `/photos/photos2025/${name}`;
 
     const verticalPairs2025 = [
             ["Mulenga2025_3.JPG",  "Mulenga2025_2.JPG"],
             ["Mulenga2025_4.JPG",  "Mulenga2025_5.JPG"],
             ["Mulenga2025_6.JPG",  "Mulenga2025_7.JPG"],
+
             ["Mulenga2025_8.JPG",  "Mulenga2025_9.JPG"],
             ["Mulenga2025_10.JPG", "Mulenga2025_12.JPG"],
             ["Mulenga2025_13.JPG", "Mulenga2025_14.JPG"],
+
             ["Mulenga2025_15.JPG", "Mulenga2025_16.JPG"],
             ["Mulenga2025_17.JPG", "Mulenga2025_18.JPG"],
             ["Mulenga2025_19.JPG", "Mulenga2025_20.JPG"],
+
             ["Mulenga2025_21.JPG", "Mulenga2025_22.JPG"],
             ["Mulenga2025_24.JPG", "Mulenga2025_26.JPG"],
             ["Mulenga2025_27.JPG", "Mulenga2025_28.JPG"],
+
             ["Mulenga2025_29.JPG", "Mulenga2025_30.JPG"],
             ["Mulenga2025_31.JPG", "Mulenga2025_32.JPG"],
             ["Mulenga2025_33.JPG", "Mulenga2025_34.JPG"],
-            ["Mulenga2025_35.JPG", "Mulenga2025_37.JPG"],
-            ["Mulenga2025_38.JPG", "Mulenga2025_40.JPG"],
-            ["Mulenga2025_42.JPG", "Mulenga2025_43.JPG"],
-            ["Mulenga2025_44.JPG", "Mulenga2025_45.JPG"],
-            ["Mulenga2025_47.JPG", "Mulenga2025_48.JPG"],
-            ["Mulenga2025_49.JPG", "Mulenga2025_50.JPG"],
-            ["Mulenga2025_51.JPG", "Mulenga2025_52.JPG"],
-            ["Mulenga2025_53.JPG", "Mulenga2025_54.JPG"],
-            ["Mulenga2025_55.JPG", "Mulenga2025_56.JPG"],
-            ["Mulenga2025_57.JPG", "Mulenga2025_58.JPG"],
-            ["Mulenga2025_59.JPG", "Mulenga2025_60.JPG"],
-            ["Mulenga2025_61.JPG", "Mulenga2025_62.JPG"],
-            ["Mulenga2025_63.JPG", "Mulenga2025_64.JPG"],
-            ["Mulenga2025_65.JPG", "Mulenga2025_66.JPG"],
-            ["Mulenga2025_67.JPG", "Mulenga2025_68.JPG"],
-            ["Mulenga2025_69.JPG", "Mulenga2025_70.JPG"],
-            ["Mulenga2025_71.JPG", "Mulenga2025_72.JPG"],
-            ["Mulenga2025_73.JPG", "Mulenga2025_74.JPG"],
-            ["Mulenga2025_75.JPG", "Mulenga2025_79.JPG"],
-            ["Mulenga2025_80.JPG", "Mulenga2025_81.JPG"],
-            ["Mulenga2025_82.JPG", "Mulenga2025_89.JPG"],
-
     ];
 
     const horizontalSingles2025 = [
@@ -53,13 +35,6 @@ export default function PicturePage() {
         "Mulenga2025_23.JPG",
         "Mulenga2025_25.JPG",
         "Mulenga2025_36.JPG",
-        "Mulenga2025_39.JPG",
-        "Mulenga2025_41.JPG",
-        "Mulenga2025_46.JPG",
-        "Mulenga2025_84.JPG",
-        "Mulenga2025_85.JPG",
-        "Mulenga2025_88.JPG",
-        "Mulenga2025_91.JPG",
     ];
 
     if (verticalPairs2025.length / horizontalSingles2025.length !== 3) {
